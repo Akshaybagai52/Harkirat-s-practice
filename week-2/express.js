5bc9b092fe5416e589a81d9967f5e4483746227f
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-06-28 16:28:32 +0530

    added cors

--- week-2/express.js
+++ week-2/express.js
@@ -1,29 +1,91 @@
 const express = require('express');
-const app = express()
-const port = 3000
+const bodyParser = require('body-parser');
+const fs = require("fs");
+const cors = require('cors');
 
-function calculateSum(counter) {
-    var sum = 0;
-    for(let i =0; i < counter; i++) {
-        sum = sum + i;
+
+const app = express();
+app.use(cors({
+  origin: "*"
+}));
+
+app.use(bodyParser.json());
+
+function findIndex(arr, id) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].id === id) return i;
+  }
+  return -1;
+}
+
+function removeAtIndex(arr, index) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i !== index) newArray.push(arr[i]);
+  }
+  return newArray;
+}
+
+app.get('/todos', (req, res) => {
+  fs.readFile("todos.json", "utf8", (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+    console.log(data);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  fs.readFile("todos.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const todos = JSON.parse(data);
+    const todoIndex = findIndex(todos, parseInt(req.params.id));
+    if (todoIndex === -1) {
+      res.status(404).send();
+    } else {
+      res.json(todos[todoIndex]);
     }
-    return sum;
-}
-function createUser(req, res) {
-    res.send("heeloos");
-}
+  });
+});
 
-function handleFirstRequest(req, res) {
-    var counter = req.query.counter;
-    var calculatedSum = calculateSum(counter);
-    var answer = "The sum is " + calculatedSum;
-    res.send(answer);
-}
+app.post('/todos', (req, res) => {
+  const newTodo = {
+    id: Math.floor(Math.random() * 1000000), // unique random id
+    title: req.body.title,
+    description: req.body.description
+  };
+  fs.readFile("todos.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const todos = JSON.parse(data);
+    todos.push(newTodo);
+    fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+      if (err) throw err;
+      res.status(201).json(newTodo);
+    });
+  });
+});
 
 
-app.get('/handleSum', handleFirstRequest );
-app.post('/createUser', createUser)
-function started() {
-    console.log(`app listening on port ${port}`)
-}
-app.listen(port, started)
+app.delete('/todos/:id', (req, res) => {
+
+  fs.readFile("todos.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const todos = JSON.parse(data);
+    const todoIndex = findIndex(todos, parseInt(req.params.id));
+    if (todoIndex === -1) {
+      res.status(404).send();
+    } else {
+      todos = removeAtIndex(todos, todoIndex);
+      fs.writeFile("todos.json", JSON.stringify(todos), (err) => {
+        if (err) throw err;
+        res.status(200).send();
+      });
+    }
+  });
+});
+
+// for all other routes, return 404
+app.use((req, res, next) => {
+  res.status(404).send();
+});
+
+app.listen(3000)
